4f1e650a6c3c793bd698cd3fdb01e06e69cbe360
igor.y <igor@heyreliable.com>
2023-11-30 19:12:11 +0200

    initial commit

--- src/js/jquery.main.js
+++ src/js/jquery.main.js
@@ -1,7 +1,11 @@
 var $ = jQuery.noConflict();
-jQuery(function() {
+jQuery(function () {
 	isElementExist(".nav-drop", initSmartMenu);
 	jcfInit();
+});
+
+jQuery(window).on("load", () => {
+	initAOS();
 });
 
 //-------- -------- -------- --------
@@ -15,57 +19,100 @@ function isElementExist(_el, _cb) {
 	if (document.body.contains(elem)) {
 		try {
 			_cb();
-		} catch(e) {
+		} catch (e) {
 			console.log(e);
 		}
 	}
 }
 
+function initAOS() {
+	// Documentation here: https://github.com/michalsnik/aos/tree/v2
+	const CONFIG = {
+		easing: "ease-out-quart",
+		offset: 0,
+		duration: 600,
+		once: true,
+	};
+
+	let delayCounter = 0;
+	const DELAY_STEP = 400; // can be any number (milliseconds)
+
+	const $aosBlocks = $("[data-aos]");
+
+	if (!$aosBlocks) return;
+
+	const inViewportBlocks = $aosBlocks.filter((_, el) => isElemVisible(el));
+
+	if (inViewportBlocks.length) {
+		// Apply sequenced animation delay for all blocks in viewport on load
+		inViewportBlocks.each((_, el) => {
+			const delayInSeconds = delayCounter ? delayCounter / 1000 : 0;
+
+			$(el).css("transition-delay", `${delayInSeconds}s`);
+
+			delayCounter += DELAY_STEP;
+		});
+	}
+
+	AOS.init(CONFIG);
+
+	function isElemVisible(el) {
+		const { top, bottom } = el.getBoundingClientRect();
+		const vHeight = window.innerHeight || document.documentElement.clientHeight;
+
+		return (top > 0 || bottom > 0) && top < vHeight;
+	}
+}
+
 // initialize custom form elements (checkbox, radio, select) https://github.com/w3co/jcf
 function jcfInit() {
-	var customSelect = jQuery('select');
+	var customSelect = jQuery("select");
 	var customCheckbox = jQuery('input[type="checkbox"]');
 	var customRadio = jQuery('input[type="radio"]');
 
 	// all option see https://github.com/w3co/jcf
-	jcf.setOptions('Select', {
+	jcf.setOptions("Select", {
 		wrapNative: false,
 		wrapNativeOnMobile: false,
 		fakeDropInBody: false,
-		maxVisibleItems: 6
+		maxVisibleItems: 6,
 	});
 
-	jcf.setOptions('Checkbox', {});
+	jcf.setOptions("Checkbox", {});
 
-	jcf.setOptions('Radio', {});
+	jcf.setOptions("Radio", {});
 
 	// init only after option
 	jcf.replace(customSelect);
 	jcf.replace(customCheckbox);
 	jcf.replace(customRadio);
-
 }
 
 // smart menu init
 function initSmartMenu() {
-	var distanceBetweenMenuAndNav = jQuery(".header-menu-wrapper").offset().top + jQuery(".header-menu-wrapper").innerHeight() - jQuery(".nav").offset().top - jQuery(".nav").innerHeight();
+	var distanceBetweenMenuAndNav =
+		jQuery(".header-menu-wrapper").offset().top +
+		jQuery(".header-menu-wrapper").innerHeight() -
+		jQuery(".nav").offset().top -
+		jQuery(".nav").innerHeight();
 	jQuery(".header-menu").smartmenus({
 		collapsibleBehavior: "accordion",
-		mainMenuSubOffsetY: distanceBetweenMenuAndNav
+		mainMenuSubOffsetY: distanceBetweenMenuAndNav,
 	});
 
 	// changed date attribute to a class (need to reverse last item menu)
-	jQuery('.header-menu').children().last().addClass('nav-sm-reverse');
+	jQuery(".header-menu").children().last().addClass("nav-sm-reverse");
 
 	jQuery("body").mobileNav({
 		menuActiveClass: "nav-active",
 		menuOpener: ".nav-opener",
 		hideOnClickOutside: true,
-		menuDrop: ".nav-drop"
-	}), "ontouchstart" in document.documentElement ||
-			jQuery(window).on("resize orientationchange", function() {
+		menuDrop: ".nav-drop",
+	}),
+		"ontouchstart" in document.documentElement ||
+			jQuery(window).on("resize orientationchange", function () {
 				jQuery("body").removeClass("nav-active"), $.SmartMenus.hideAll();
-	});
+			});
 }
 
 //-------- -------- -------- --------
@@ -84,6 +131,9 @@ function initSmartMenu() {
 
 // jcf library select, radio, checkbox modules
 //= vendors/jcf.js
+
+// aos library for animations on scroll
+//= vendors/aos.min.js
 
 //-------- -------- -------- --------
 //-------- included js libs end
